cc85970ff4328f8a1ee17439ed0de64a4c8ec7b0
VictorDoyle <victor-doyle@outlook.com>
2023-11-14 16:14:30 -0500

    added replacements for common descriptors in docs

--- src/utils/docExtractor.ts
+++ src/utils/docExtractor.ts
@@ -16,7 +16,25 @@ export function extractDocsFromFiles(files: string[]): Record<string, string> {
             // Process @param and @returns
             doc = doc.replace(/@param {(\w+)} (\w+) - ([\s\S]*?)(?=(\n \* @|$))/g, 'Parameter: $2 - $1\n    - $3\n');
             doc = doc.replace(/@returns {(\w+)} - ([\s\S]*?)(?=(\n \* @|$))/g, 'Expected Return: $1 - $2\n');
-
+            doc = doc.replace(/@type {(\w+)} - ([\s\S]*?)(?=(\n \* @|$))/g, 'Type: $1 - $2\n');
+            doc = doc.replace(/@example(.*?)((?=(\n \* @|$)))/gs, 'Example: $1\n');
+            doc = doc.replace(/@remarks(.*?)((?=(\n \* @|$)))/gs, 'Remarks: $1\n');
+            doc = doc.replace(/@see(.*?)((?=(\n \* @|$)))/gs, 'See: $1\n');
+            doc = doc.replace(/@deprecated(.*?)((?=(\n \* @|$)))/gs, 'Deprecated: $1\n');
+            doc = doc.replace(/@throws {(\w+)} - ([\s\S]*?)(?=(\n \* @|$))/g, 'Throws: $1 - $2\n');
+            doc = doc.replace(/@private/g, '**Private**\n');
+            doc = doc.replace(/@public/g, '**Public**\n');
+            doc = doc.replace(/@protected/g, '**Protected**\n');
+            doc = doc.replace(/@abstract/g, '**Abstract**\n');
+            doc = doc.replace(/@constructor/g, '**Constructor**\n');
+            doc = doc.replace(/@static/g, '**Static**\n');
+            doc = doc.replace(/@author(.*?)((?=(\n \* @|$)))/gs, 'Author: $1\n');
+            doc = doc.replace(/@version(.*?)((?=(\n \* @|$)))/gs, 'Version: $1\n');
+            doc = doc.replace(/@since(.*?)((?=(\n \* @|$)))/gs, 'Since: $1\n');
+            doc = doc.replace(/@todo(.*?)((?=(\n \* @|$)))/gs, 'Todo: $1\n');
+            doc = doc.replace(/@license(.*?)((?=(\n \* @|$)))/gs, 'License: $1\n');
+            doc = doc.replace(/@property {(\w+)} (\w+) - ([\s\S]*?)(?=(\n \* @|$))/g, 'Property: $2 - $1\n    - $3\n');
+            
             // Process @public and @private
             const visibility = doc.includes('@public') ? '**Public**' : doc.includes('@private') ? '**Private**' : '';
             doc = visibility + '\n' + doc.replace(/@(public|private)/g, '');
